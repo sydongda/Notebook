1759c3f6b7cd3497e9e83d12a195752beb87eaee
sydongda <sydongda@qq.com>
2017-11-08 22:48:20 -0600

    Update reset-volume-online-music-1ting-website.js

--- reset-volume-online-music-1ting-website.js
+++ reset-volume-online-music-1ting-website.js
@@ -1,21 +1,18 @@
-function reset_volume_1ting() {
+timeout = 2000;
+function adjuest_volume_ting() {
     setTimeout(function() {
-    	while (true) {
-    	var bgp = $("#volume-crontrol").css("background-position");
-    	var min = bgp.split("-")[0];
-    	if (min.substring(0, min.length - 3) > 10) {
-    		$("#volume-min").trigger("click");
-    	} else if (min.substring(0, min.length - 3)  == 0) {
-    	 	$("#volume-max").trigger("click");
-    	 	break;
-    	 } else {
-    	 	break;
-    	 }
-    	 }
-    	reset_volume_1ting();
-      }, 
-	  2000);
+        var bgp = $("#volume-crontrol").css("background-position");
+        var min = bgp.split("-")[0];
+        var v = min.substring(0, min.length - 3);
+        console.log(v +"," + timeout);
+        timeout = 2000;
+        if (v > 10) { 
+            $("#volume-min").trigger("click");
+            timeout = 100;
+        } else if (v  == 0) {
+            $("#volume-max").trigger("click");
+         }
+        adjuest_volume_ting();
+    }, timeout);
 }
-reset_volume_1ting();
-    
-    
+adjuest_volume_ting();

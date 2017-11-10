d24072a5a3683a70c4c66df0317f8b7e8ab4a43f
sydongda <sydongda@qq.com>
2017-11-09 19:56:46 -0600

    Update reset-volume-online-music-1ting-website.js
    comment log

--- reset-volume-online-music-1ting-website.js
+++ reset-volume-online-music-1ting-website.js
@@ -3,7 +3,7 @@ function adjuest_volume_ting() {
     setTimeout(function() {
         var position = $("#volume-crontrol").css("background-position");
         var v = position.substring(0, position.indexOf("px"));
-        console.log(v +"," + timeout);
+        //console.log(v +"," + timeout);
         timeout = 2000;
         if (v > 10) { 
             $("#volume-min").trigger("click");

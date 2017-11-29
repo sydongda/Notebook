e96b264b63427cedbabf8491d827d36946db0b6f
sydongda <sydongda@qq.com>
2017-11-29 14:45:43 +0800

    replace jquery api with javascript html's

--- reset-volume-online-music-1ting-website.js
+++ reset-volume-online-music-1ting-website.js
@@ -1,16 +1,15 @@
 timeout = 2000;
 function adjuest_volume_ting() {
     setTimeout(function() {
-        var position = $("#volume-crontrol").css("background-position");
+        var position = document.getElementById("volume-crontrol").style["background-position"];
         var v = position.substring(0, position.indexOf("px"));
-        //console.log(v +"," + timeout);
         timeout = 2000;
         if (v > 10) { 
-            $("#volume-min").trigger("click");
+            document.getElementById("volume-min").click();
             timeout = 100;
         } else if (v  == 0) {
-            $("#volume-max").trigger("click");
-         }
+            document.getElementById("volume-max").click();
+        }
         adjuest_volume_ting();
     }, timeout);
 }
